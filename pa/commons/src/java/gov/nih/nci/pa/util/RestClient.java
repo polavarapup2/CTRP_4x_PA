ba0fbdba5e85ef85c46fc0faf38704c62c181045
purnimac <purnimac@yahoo.com>
2017-03-01 12:24:34 -0500

    RestClient refactoring

--- pa/commons/src/java/gov/nih/nci/pa/util/RestClient.java
+++ pa/commons/src/java/gov/nih/nci/pa/util/RestClient.java
@@ -1,7 +1,5 @@
 package gov.nih.nci.pa.util;
 
-import gov.nih.nci.pa.service.PAException;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -14,8 +12,10 @@ import javax.net.ssl.SSLContext;
 import javax.net.ssl.TrustManager;
 import javax.net.ssl.X509TrustManager;
 
+import org.apache.commons.lang.StringUtils;
 import org.apache.log4j.Logger;
 
+import gov.nih.nci.pa.service.PAException;
 
 /**
  * @author Purnima, Reshma
@@ -29,7 +29,7 @@ public class RestClient {
     private static final Integer HTTP_SUCCESS_CODE_201 = 201;
     private static final int RETRY_COUNT = 3;
     private static final int HTTP_TIME_OUT = 10000;
-
+    
     /**
      * 
      *
@@ -37,41 +37,40 @@ public class RestClient {
      *            - enables to allow all tursted sites
      */
 
-     public RestClient(boolean allowTrustedSites) {
+    public RestClient(boolean allowTrustedSites) {
         if (allowTrustedSites) {
             trustAllCerts();
         }
     }
+    
+    /**
+     * trusts all certificates
+     */
+    private void trustAllCerts() {
 
+        // Create a trust manager that does not validate certificate chains
+        TrustManager[] trustAllCerts = new TrustManager[] {new X509TrustManager() {
+            public java.security.cert.X509Certificate[] getAcceptedIssuers() { // NOPMD
+                return null;
+            }
 
-     /**
-      * trusts all certificates
-      */
-     private void trustAllCerts() {
+            public void checkClientTrusted(java.security.cert.X509Certificate[] certs, String authType) {
+                // TODO
+            }
 
-         // Create a trust manager that does not validate certificate chains
-         TrustManager[] trustAllCerts = new TrustManager[] {new X509TrustManager() {
-             public java.security.cert.X509Certificate[] getAcceptedIssuers() { // NOPMD
-                 return null;
-             }
-
-             public void checkClientTrusted(java.security.cert.X509Certificate[] certs, String authType) {
-                 //TODO
-             }
-
-             public void checkServerTrusted(java.security.cert.X509Certificate[] certs, String authType) {
-                 //TODO
-             }
-         } };
-         // Install the all-trusting trust manager
-         try {
-             SSLContext sc = SSLContext.getInstance("SSL");
-             sc.init(null, trustAllCerts, new java.security.SecureRandom());
-             HttpsURLConnection.setDefaultSSLSocketFactory(sc.getSocketFactory());
-         } catch (Exception e) {
-             LOG.error(e.getMessage(), e);
-         }
-     }
+            public void checkServerTrusted(java.security.cert.X509Certificate[] certs, String authType) {
+                // TODO
+            }
+        } };
+        // Install the all-trusting trust manager
+        try {
+            SSLContext sc = SSLContext.getInstance("SSL");
+            sc.init(null, trustAllCerts, new java.security.SecureRandom());
+            HttpsURLConnection.setDefaultSSLSocketFactory(sc.getSocketFactory());
+        } catch (Exception e) {
+            LOG.error(e.getMessage(), e);
+        }
+    }
 
     /**
      * Invokes REST api to get the
@@ -83,80 +82,93 @@ public class RestClient {
      * @param postBody
      *            - Post body
      * @return - Response after update
-     * @throws PAException PAException
+     * @throws PAException
+     *             PAException
      * 
      */
     @SuppressWarnings({ "PMD.CyclomaticComplexity" })
-    public String sendHTTPRequest(String restUrl, String method, String postBody)
-            throws PAException {
+    public String sendHTTPRequest(String restUrl, String method, String postBody) throws PAException {
         int httpResponseCode = -1;
         String httpResponseMessage = "";
         LOG.debug("url: " + restUrl);
-        StringBuilder httpResponse = new StringBuilder();
-        BufferedReader reader = null;
         HttpURLConnection urlConnection = null;
 
         boolean success = false;
         for (int count = 0; count <= RETRY_COUNT; count++) {
             try {
-                URL url = new URL(restUrl);
-                urlConnection = (HttpURLConnection) url.openConnection();
-                urlConnection.setConnectTimeout(HTTP_TIME_OUT);
-                urlConnection.setReadTimeout(HTTP_TIME_OUT);
-                urlConnection.setRequestMethod(method);
-                urlConnection.setDoOutput(true);
-                urlConnection.setRequestProperty("Content-Type",
-                        "text/xml;charset=UTF-8");
-
-                if (postBody != null && postBody.length() > 0) {
-                    LOG.debug("postBody: " + postBody);
-                    setPostBody(urlConnection, postBody);
-                }
+                urlConnection = makeUrlConnection(restUrl, method, postBody);
 
                 httpResponseCode = urlConnection.getResponseCode();
-                httpResponseMessage = urlConnection.getResponseMessage();
-                if (httpResponseCode == HTTP_SUCCESS_CODE_200
-                        && httpResponseCode == HTTP_SUCCESS_CODE_201) {
+                if (httpResponseCode == HTTP_SUCCESS_CODE_200 || httpResponseCode == HTTP_SUCCESS_CODE_201) {
                     success = true;
                     break;
                 }
             } catch (Exception e) {
-                LOG.error("Error: Unable to get response from Rest server ("
-                        + httpResponseCode + ") - " + httpResponseMessage, e);
+                LOG.error("Error: Unable to get response from Rest server (" + httpResponseCode + ") - "
+                        + httpResponseMessage, e);
             }
         }
         if (success) {
-            try {
-                reader = new BufferedReader(new InputStreamReader(
-                        urlConnection.getInputStream()));
-                String line;
-
-                while ((line = reader.readLine()) != null) {
-                    httpResponse.append(line);
-                }
-                if (reader != null) {
-                    try {
-                        reader.close();
-                    } catch (IOException e) {
-                        LOG.error(e.getMessage(), e);
-                    }
-                }
-
-                if (urlConnection != null) {
-                    urlConnection.disconnect();
-                }
-            } catch (Exception e) {
-                throw new PAException(
-                        "Error in reading micro service response:  "
-                                + e.getMessage(), e);
-            }
-            return httpResponse.toString();
+            return readResponse(urlConnection);
         } else {
-            throw new PAException(
-                    "Error: Unable to get response from Rest server @"
-                            + restUrl);
+            throw new PAException("Error: Unable to get response from Rest server @" + restUrl);
         }
 
+    }
+
+    private String readResponse(HttpURLConnection urlConnection) throws PAException {
+        BufferedReader reader;
+        StringBuilder httpResponse = new StringBuilder();
+        try {
+            reader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
+            String line;
+
+            while ((line = reader.readLine()) != null) {
+                httpResponse.append(line);
+            }
+            if (reader != null) {
+                try {
+                    reader.close();
+                } catch (IOException e) {
+                    LOG.error(e.getMessage(), e);
+                }
+            }
+
+            if (urlConnection != null) {
+                urlConnection.disconnect();
+            }
+        } catch (Exception e) {
+            throw new PAException("Error in reading micro service response:  " + e.getMessage(), e);
+        }
+        return httpResponse.toString();
+    }
+
+    /**
+     * 
+     * @param restUrl the URL to connect to
+     * @param method the HTTP method GET/POST/PUT/DELETE
+     * @param postBody the jsonStr of the post request body
+     * @return http connection to the URL 
+     * @throws MalformedURLException
+     * @throws IOException
+     * @throws ProtocolException
+     */
+    private HttpURLConnection makeUrlConnection(String restUrl, String method, String postBody)
+            throws IOException {
+        URL url = new URL(restUrl);
+        HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+        urlConnection.setConnectTimeout(HTTP_TIME_OUT);
+        urlConnection.setReadTimeout(HTTP_TIME_OUT);
+        urlConnection.setRequestMethod(method);
+        urlConnection.setRequestProperty("Accept", "application/json");
+        urlConnection.setDoOutput(true);
+
+        if (StringUtils.equals(method, "POST") && postBody != null && postBody.length() > 0) {
+            LOG.debug("postBody: " + postBody);
+            urlConnection.setRequestProperty("Content-Type", "application/json");
+            setPostBody(urlConnection, postBody);
+        }
+        return urlConnection;
     }
 
     /**
@@ -169,14 +181,10 @@ public class RestClient {
      * @throws IOException
      *             - throws IOException
      */
-    private void setPostBody(HttpURLConnection urlConnection, String postBody)
-            throws IOException {
-
+    private void setPostBody(HttpURLConnection urlConnection, String postBody) throws IOException {
         OutputStream outputStream = urlConnection.getOutputStream();
         outputStream.write(postBody.getBytes());
         outputStream.flush();
     }
 
-
 }
-
