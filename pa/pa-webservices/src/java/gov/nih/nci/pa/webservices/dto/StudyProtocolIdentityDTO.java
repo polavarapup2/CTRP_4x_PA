00937cfd7d80aec295cac684c44619927ebebc75
Reshmakoganti <reshma.koganti@semanticbits.com>
2017-03-23 10:46:35 -0400

    DTO change for CTGOV import

---
+++ pa/pa-webservices/src/java/gov/nih/nci/pa/webservices/dto/StudyProtocolIdentityDTO.java
@@ -0,0 +1,137 @@
+package gov.nih.nci.pa.webservices.dto;
+
+import java.util.List;
+
+import org.codehaus.jackson.annotate.JsonIgnoreProperties;
+
+/**
+ * 
+ * @author Reshma
+ *
+ */
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class StudyProtocolIdentityDTO {
+    private String nciId;
+    private String studyProtocolId;
+    private String nctId;
+    private String userLastCreated;
+    private String trialType;
+    private String currentUser;
+    private boolean isUpdate;
+    private List<String> secondaryIdentifiers;
+    /**
+     * 
+     * @return nciId
+     */
+    public String getNciId() {
+        return nciId;
+    }
+    /**
+     * 
+     * @param nciId the nciId
+     */
+    public void setNciId(String nciId) {
+        this.nciId = nciId;
+    }
+    /**
+     * 
+     * @return studyProtocolId 
+     */
+    public String getStudyProtocolId() {
+        return studyProtocolId;
+    }
+    /**
+     * 
+     * @param studyProtocolId the studyProtocolId
+     */
+    public void setStudyProtocolId(String studyProtocolId) {
+        this.studyProtocolId = studyProtocolId;
+    }
+    /**
+     * 
+     * @return nctId
+     */
+    public String getNctId() {
+        return nctId;
+    }
+    /**
+     * 
+     * @param nctId the nctId
+     */
+    public void setNctId(String nctId) {
+        this.nctId = nctId;
+    }
+    /**
+     * 
+     * @return userLastCreated 
+     */
+    public String getUserLastCreated() {
+        return userLastCreated;
+    }
+    /**
+     * 
+     * @param userLastCreated the userLastCreated
+     */
+    public void setUserLastCreated(String userLastCreated) {
+        this.userLastCreated = userLastCreated;
+    }
+    /**
+     * 
+     * @return trialType
+     */
+    public String getTrialType() {
+        return trialType;
+    }
+    /**
+     * 
+     * @param trialType the trialType
+     */
+    public void setTrialType(String trialType) {
+        this.trialType = trialType;
+    }
+    /**
+     * 
+     * @return currentUser
+     */
+    public String getCurrentUser() {
+        return currentUser;
+    }
+    /**
+     * 
+     * @param currentUser the currentUser
+     */
+    public void setCurrentUser(String currentUser) {
+        this.currentUser = currentUser;
+    }
+    /**
+     * 
+     * @return isUpdate
+     */
+    public boolean isUpdate() {
+        return isUpdate;
+    }
+    /**
+     * 
+     * @param isUpdate the isUpdate
+     */
+    public void setUpdate(boolean isUpdate) {
+        this.isUpdate = isUpdate;
+    }
+    /**
+     * 
+     * @return secondaryIdentifiers
+     */
+    public List<String> getSecondaryIdentifiers() {
+        return secondaryIdentifiers;
+    }
+    /**
+     * 
+     * @param secondaryIdentifiers the secondaryIdentifiers
+     */
+    public void setSecondaryIdentifiers(List<String> secondaryIdentifiers) {
+        this.secondaryIdentifiers = secondaryIdentifiers;
+    }
+    
+    
+    
+}
