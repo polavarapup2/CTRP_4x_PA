90969e44be30e2abd969832bd4277b7696f478ac
briannorris2 <briannorris2@gmail.com>
2017-04-07 11:32:25 -0400

    fixing Grants query issue

--- pa/pa-ejb/src/java/gov/nih/nci/pa/service/util/I2EGrantsServiceBean.java
+++ pa/pa-ejb/src/java/gov/nih/nci/pa/service/util/I2EGrantsServiceBean.java
@@ -4,6 +4,8 @@ import gov.nih.nci.coppa.services.interceptor.RemoteAuthorizationInterceptor;
 import gov.nih.nci.pa.service.PAException;
 import gov.nih.nci.pa.util.PaHibernateSessionInterceptor;
 import org.apache.commons.dbutils.DbUtils;
+import org.apache.commons.dbutils.QueryRunner;
+import org.apache.commons.dbutils.ResultSetHandler;
 import org.apache.commons.lang.StringUtils;
 
 import javax.ejb.EJB;
@@ -34,12 +36,12 @@ public class I2EGrantsServiceBean implements I2EGrantsServiceLocal {
     private static final String SQL_SEARCH =
             "SELECT serial_number, institution_name, project_title, pi_first_name, pi_last_name "
             + "FROM grants "
-            + "WHERE CAST(SERIAL_NUMBER AS TEXT) LIKE ? ";
+            + "WHERE serial_number::text LIKE ? ";
 
     private static final String SQL_VALIDATE =
             "SELECT COUNT(*) "
             + "FROM grants "
-            + "WHERE serial_number = ?";
+            + "WHERE serial_number::text = ?";
 
     @EJB
     private LookUpTableServiceRemote lookUpTableSvc;
@@ -56,30 +58,28 @@ public class I2EGrantsServiceBean implements I2EGrantsServiceLocal {
         if (data == null) {
             return new ArrayList<I2EGrant>();
         }
-
-        List<I2EGrant> result = new ArrayList<I2EGrant>();
-        Connection connection = getConnection();
-        PreparedStatement stmt = null;
-        ResultSet rs = null;
-        try {
-            stmt = connection.prepareStatement(SQL_SEARCH);
-            stmt.setString(1, data + "%");
-            rs = stmt.executeQuery();
-
-            while (rs.next()) {
-                I2EGrant grant = new I2EGrant();
-                grant.setSerialNumber(String.valueOf(rs.getObject(COL_SN)));
-                grant.setOrganizationName((String) rs.getObject(COL_INST));
-                grant.setProjectName((String) rs.getObject(COL_PROJ));
-                grant.setPiFirstName((String) rs.getObject(COL_FIRST));
-                grant.setPiLastName((String) rs.getObject(COL_LAST));
-                result.add(grant);
+        ResultSetHandler<List<I2EGrant>> handler = new ResultSetHandler<List<I2EGrant>>() {
+            public List<I2EGrant> handle(ResultSet rs) throws SQLException {
+                List<I2EGrant> result = new ArrayList<I2EGrant>();
+                while (rs.next()) {
+                    I2EGrant grant = new I2EGrant();
+                    grant.setSerialNumber(String.valueOf(rs.getObject(COL_SN)));
+                    grant.setOrganizationName(String.valueOf(rs.getObject(COL_INST)));
+                    grant.setProjectName(String.valueOf(rs.getObject(COL_PROJ)));
+                    grant.setPiFirstName(String.valueOf(rs.getObject(COL_FIRST)));
+                    grant.setPiLastName(String.valueOf(rs.getObject(COL_LAST)));
+                    result.add(grant);
+                }
+                return result;
             }
+        };
+        QueryRunner run = new QueryRunner();
+        List<I2EGrant> result = new ArrayList<>();
+        Connection connection = getConnection();
+        try {
+            result = run.query(connection, SQL_SEARCH, handler, data + "%");
         } catch (SQLException e) {
             throw new PAException("Error querying Postgres database.", e);
-        } finally {
-            DbUtils.closeQuietly(rs);
-            DbUtils.closeQuietly(stmt);
         }
         return result;
     }
