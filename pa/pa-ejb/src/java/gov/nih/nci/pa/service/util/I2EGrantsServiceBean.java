5225532aced201457d638422e61901b46bfd8938
briannorris2 <briannorris2@gmail.com>
2017-04-20 15:45:39 -0400

    fixing issue with Grants returning too many records

--- pa/pa-ejb/src/java/gov/nih/nci/pa/service/util/I2EGrantsServiceBean.java
+++ pa/pa-ejb/src/java/gov/nih/nci/pa/service/util/I2EGrantsServiceBean.java
@@ -36,7 +36,7 @@ public class I2EGrantsServiceBean implements I2EGrantsServiceLocal {
     private static final String SQL_SEARCH =
             "SELECT serial_number, institution_name, project_title, pi_first_name, pi_last_name "
             + "FROM grants "
-            + "WHERE serial_number::text LIKE ? ";
+            + "WHERE serial_number::text LIKE ? LIMIT 10";
 
     private static final String SQL_VALIDATE =
             "SELECT COUNT(*) "
